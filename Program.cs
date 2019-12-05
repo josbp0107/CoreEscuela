17fbf15431a290cde40f1b2b7116b9612cfe72a7
Jose B. Paternina <josebp0107@gmail.com>
2019-12-05 17:29:45 -0500

    Empezando a programar en C# NET Core

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo;
+        public void Timbrar()
+        {
+            //Todo
+            Console.Beep(10000, 3000);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Liceo Panamericano";
+            miEscuela.direccion = "Cra 23 #23-123";
+            miEscuela.añoFundacion = 1998;
+            Console.WriteLine("TIMBRA");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
