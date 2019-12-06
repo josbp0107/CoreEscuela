0714820e23e112da7ac789862e817c0b3f4dc174
josbp0107 <josebp0107@gmail.com>
2019-12-05 22:24:06 -0500

    Se agrego una nueva clase llamado Curso el cuanto contendra cada atributo de esta

--- Program.cs
+++ Program.cs
@@ -8,7 +8,24 @@ namespace CoreEscuela
         static void Main(string[] args)
         {
             var escuela = new Escuela("Liceo", 2010, TiposEscuela.Primaria, pais:"Colombia", ciudad:"Sincelejo");
+            var curso = new Curso()
+            {
+                Nombre = "101"
+            };
+            var curso2 = new Curso()
+            {
+                Nombre = "201"
+            };
+            var curso3 = new Curso()
+            {
+                Nombre = "301"
+            };
+
             Console.WriteLine(escuela);
+            System.Console.WriteLine("##############");
+            Console.WriteLine(curso.Nombre+","+ curso.UniqueId);
+            Console.WriteLine(curso2.Nombre+","+ curso2.UniqueId);
+            Console.WriteLine(curso3.Nombre+","+ curso3.UniqueId);
         }
     }
 }
