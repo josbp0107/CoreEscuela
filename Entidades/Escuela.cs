ea123929a3c2b9d9096614fc91c8f749a2f64445
josbp0107 <josebp0107@gmail.com>
2019-12-05 20:56:18 -0500

    Agregando constructor y modificadores de acceso

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,35 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre; // Atributo de la entidad Escuela
+        
+        //Atributo Nombre encapsulado con sus diferentes modificadores de acceso
+        public string Nombre  
+        {
+            get{ return nombre; }
+            set{ nombre = value.ToUpper(); }
+        }
+
+        public int AñoDeCreacion{ get;set; } //Modificadores de acceso encapsulados
+        public string Pais { get; set; }  
+        public string Ciudad { get; set; }
+
+        /*
+            Metodo constructor de la clase Escuela 
+            @Param string nombre atributo de la entidad Escuela 
+            @param int año atributo de la entidad Escuela
+        */
+
+        // public Escuela(string nombre, int año)
+        // {
+        //     this.nombre = nombre;
+        //     AñoDeCreacion = año;
+        // }
+
+        // Otra manera de crear constructor de una clase
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año); //Igualación por tuplas
+
+           
+    }
+}
