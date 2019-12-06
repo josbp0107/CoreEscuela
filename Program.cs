28f5e34131302d27df3eb334f566e2461d3ad4d9
josbp0107 <josebp0107@gmail.com>
2019-12-06 17:58:17 -0500

    Agregue expresion lamda para eliminar un objeto de la lista

--- Program.cs
+++ Program.cs
@@ -17,12 +17,27 @@ namespace CoreEscuela
                     new Curso { Nombre = "301", Jornada= TiposJornada.Mañana }
             };
 
-            escuela.Cursos.Add(new Curso() { Nombre = "102", Jornada = TiposJornada.Tarde });
-            escuela.Cursos.Add(new Curso() { Nombre = "202", Jornada = TiposJornada.Tarde });
+            escuela.Cursos.Add(new Curso() { Nombre = "501", Jornada = TiposJornada.Mañana });
+            escuela.Cursos.Add(new Curso() { Nombre = "501", Jornada = TiposJornada.Tarde });
+            ImprimirCursosEscuelas(escuela);
 
+            escuela.Cursos.RemoveAll(delegate (Curso cur)
+            {
+                return cur.Nombre == "301";
+            });
+
+            escuela.Cursos.RemoveAll((Curso cur) => cur.Nombre == "501" && cur.Jornada == TiposJornada.Tarde);//Lamda para eliminar un objeto o varios objetos pasandole un atributo 
+
+
+            WriteLine("===========");
             ImprimirCursosEscuelas(escuela);
 
         }
+
+        // private static bool Predicado(Curso obj)
+        // {
+        //     return obj.Nombre == "301";
+        // }
 
         private static void ImprimirCursosEscuelas(Escuela escuela)
         {
@@ -40,41 +55,5 @@ namespace CoreEscuela
 
         }
 
-        private static void ImprimirCursos(Curso[] arregloCursos)
-        {
-
-            int contador = 0;
-            while (contador < arregloCursos.Length)
-            {
-                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
-                contador++;
-            }
-        }
-
-        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
-        {
-            int contador = 0;
-            do
-            {
-                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
-                contador++;
-            } while (contador < arregloCursos.Length);
-        }
-
-        private static void ImprimirCursosFor(Curso[] arregloCursos)
-        {
-            for (int i = 0; i < arregloCursos.Length; i++)
-            {
-                Console.WriteLine($"Nombre {arregloCursos[i].Nombre}, Id {arregloCursos[i].UniqueId}");
-            }
-        }
-
-        private static void ImprimirCursosForEach(Curso[] arregloCursos)
-        {
-            foreach (var curso in arregloCursos)
-            {
-                Console.WriteLine($"Nombre {curso.Nombre}, Id {curso.UniqueId}");
-            }
-        }
     }
 }
